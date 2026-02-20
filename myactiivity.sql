283a55a277af4d1e0a81f7e9776de2d24c0e5c7f
rehan123coder <rehankhann@gmail.com>
2026-02-20 17:31:41 +0500

    sql

--- myactiivity.sql
+++ myactiivity.sql
@@ -7,6 +7,9 @@ school varchar(25)
 INSERT INTO student(student_id,name,city,school)
 VALUES(1,"rehan","lahore","opf"),
 (2,"ehan","lahore","opf"),
-(3,"han","lahore","opf");
+(3,"han","pindi","opf");
 SELECT *FROM student;
 SELECT* FROM student WHERE student_id=2;
+SELECT* FROM student WHERE name='rehan' or school='opf';
+UPDATE student SET name='asx'WHERE student_id=3;
+DELETE FROM student WHERE name='ehan';
