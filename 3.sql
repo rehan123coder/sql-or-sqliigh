ef4d8e09f0164ded3a068f914717ed8e0b619da6
rehan123coder <rehankhann@gmail.com>
2026-03-06 14:45:44 +0500

    sql

---
+++ 3.sql
@@ -0,0 +1,13 @@
+CREATE TABLE (empelye
+empelye_id int,
+salary varchar(255),
+post varchar(255),
+contract varchar(25)
+);
+INSERT INTO student(empelye_id,salary,post,contract)
+VALUES(1,"rehan","1000000","4y"),
+(2,"ehan","100000","4y"),
+(3,"han","2000000","10y");
+SELECT *FROM empelye;
+SELECT* FROM empelye WHERE empelye_id=2;
+SELECT* FROM student WHERE empelye='rehan' or contract='4y';
