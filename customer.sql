88b580dc7bd53945ff820d61f5a2e784e2312c12
rehan123coder <rehankhann@gmail.com>
2026-03-13 17:30:40 +0500

    sql

--- customer.sql
+++ customer.sql
@@ -13,3 +13,8 @@ VALUES (1,'FARHAN','PAKISTAN','LAHORE'),
 SELECT * FROM customer;
 SELECT * FROM customer ORDER BY customer_name DESC;
 SELECT COUNT(cust_id),customer_country FROM customer GROUP BY customer_country;
+INSERT INTO customer(cust_id,customer_name ,customer_country,customer_area)
+VALUES (1,'FARHAN','NULL','wasngton')
+SELECT * FROM customer WHERE customer_country IS NULL;
+SELECT * FROM customer WHERE cust_id BETWEEN 2 AND 4 ;
+SELECT * FROM customer WHERE customer_country IS NULL;
