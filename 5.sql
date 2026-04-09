ac218d4bab5af60f0e0d5f68255d2a739b6c2fdc
rehan123coder <rehankhann@gmail.com>
2026-04-09 17:46:22 +0500

    sql

---
+++ 5.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS RESTRAUNT (
+name TEXT,
+review REAL,
+price TEXT,
+health TEXT
+);
+INSERT INTO RESTRAUNT (name,review,price,health)
+VALUES
+('KFC',4.4,'$$','A'),
+('CHEEZIOUS',4.6,'$','A+'),
+('DOPE',4.5,'$','A')
+('WILD WINGS ',4.1,'$','B')
+('TAYTO',4.0,'$','B+')
+SELECT * FROM RESTRAUNT;
